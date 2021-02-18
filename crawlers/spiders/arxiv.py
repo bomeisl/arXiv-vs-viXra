51e5fb8cbd5b325dd83b2e164fcd74a3d36dc010
Kyle Bomeisl <45512800+bomeisl@users.noreply.github.com>
2021-02-18 18:01:59 -0500

    Update arxiv.py

--- crawlers/spiders/arxiv.py
+++ crawlers/spiders/arxiv.py
@@ -3,8 +3,21 @@ import logging as logger
 import click
 from colorama import init, Fore
 import feedparser
+import os
+from collections import namedtuple
 
-from crawlers.spiders.utils import ArticleData, dump_article
+import simplejson
+
+ArticleData = namedtuple('ArticleData', ['id', 'title', 'abstract', 'category'])
+
+
+def dump_article(data, datadir):
+    assert isinstance(data, ArticleData)
+    filename = data.id.replace(':', '_') + '.json'
+    path = os.path.join(datadir, filename)
+    with open(path, 'w') as buf:
+        simplejson.dump(dict(data._asdict()), buf)
+        
 from tqdm import tqdm
 
 init()
