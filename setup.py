938ee44944d1a701e1058f4c9cece631f30ea27f
Kyle Bomeisl <45512800+bomeisl@users.noreply.github.com>
2021-02-18 09:59:56 -0500

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="vixra-fork-KB", # Replace with your own username
+    version="0.0.1",
+    author="KB",
+    author_email="kaybomeisl2@gmail.com",
+    description="Vixra-scraper-fork",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dsuess/arXiv-vs-viXra.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.6',
+)
