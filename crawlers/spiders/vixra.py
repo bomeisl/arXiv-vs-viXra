a906d68c7858298c9543647a7c2477016be60078
Kyle Bomeisl <45512800+bomeisl@users.noreply.github.com>
2021-02-18 18:01:24 -0500

    Update vixra.py

--- crawlers/spiders/vixra.py
+++ crawlers/spiders/vixra.py
@@ -4,7 +4,20 @@ from colorama import Fore, init
 
 import scrapy
 
-from .utils import ArticleData, dump_article
+import os
+from collections import namedtuple
+
+import simplejson
+
+ArticleData = namedtuple('ArticleData', ['id', 'title', 'abstract', 'category'])
+
+
+def dump_article(data, datadir):
+    assert isinstance(data, ArticleData)
+    filename = data.id.replace(':', '_') + '.json'
+    path = os.path.join(datadir, filename)
+    with open(path, 'w') as buf:
+        simplejson.dump(dict(data._asdict()), buf)
 
 init()
 
